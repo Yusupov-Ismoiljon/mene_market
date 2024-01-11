1795c50ca513d8f696096362b14dc18c485a8a8a
tolamirzayev-077 <mirodiltolamirzayev5@gmail.com>
2024-01-11 21:35:06 +0500

    Log in and signup add style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,26 +11,26 @@ const Login = ({ signup, setSignup }) => {
     return (
         <>
             <div>
-                <div className='flex justify-center items-center mb-10'>
-                    <div className='w-[450px] border-[#0B0B0B] rounded-lg show py-7 px-7'>
+                <div className='flex justify-center items-center '>
+                    <div className='w-[450px] border-[#0B0B0B] rounded-lg show py-6 px-7'>
                         <Card className='flex text-center' color="transparent" shadow={false}>
                             <h2 className='font-bold text-2xl leading-8 text-[#242424]'>
-                                Авторизация
+                                Ruxsat
                             </h2>
                             <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
                                 <div className="mb-4 flex flex-col gap-4">
-                                    <Input className='block' size="lg" type='email' label="Почта" required />
-                                    <Input className='block' type="password" size="lg" label="Пароль" required />
+                                    <Input className='block' size="lg" type='email' label="Pochta" required />
+                                    <Input className='block' type="password" size="lg" label="Parol" required />
                                 </div>
-                                <Button type='submit' className="mt-7 bg-[#1B37A3]" fullWidth>
-                                    Войти
+                                <Button type='submit' className="mt-7 bg-[#3b8cdc]" fullWidth>
+                                    Kirish
                                 </Button>
-                                <Typography color="gray" className="mt-4 text-center font-normal">
+                                <Typography color="gray" className="mt-4 text-center space-x-5 font-normal">
                                     <span>
-                                        Ещё нет аккаунта?
+                                        Hali hisobingiz yo`qmi ?
                                     </span>
                                     <Link to={'/signup'} className="font-medium logo ml-2">
-                                        Зарегистрироваться
+                                        Roʻyxatdan oʻtish
                                     </Link>
                                 </Typography>
                             </form>
