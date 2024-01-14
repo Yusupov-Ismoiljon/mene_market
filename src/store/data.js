8434873b45dd5649239f4483f74b7ca8bf950274
tolamirzayev-077 <mirodiltolamirzayev5@gmail.com>
2024-01-14 13:52:11 +0500

    Add site product data

--- src/store/data.js
+++ src/store/data.js
@@ -1,6 +1,61 @@
+import Headphones from "../../public/img/Headphones.png"
+import Watch from "../../public/img/Watch.png"
+import Mouse from "../../public/img/Mouse.png"
+import Iphone from "../../public/img/iPhone.png"
+import Pot from "../../public/img/Pot.png"
+import Mug from "../../public/img/Mug.png"
+import LuxoryPot from "../../public/img/LuxoryPot.png"
+import WallPot from "../../public/img/WallPot.png"
+
+
 export const YangiMahsulot = [
     {
-        
-    }
+        id: 1,
+        img: Headphones,
+        title: "Headphones",
+        price: 40
+    },
+    {
+        id: 2,
+        img: Watch,
+        title: "Watch 7 Prime Copy",
+        price: 400
+    },
+    {
+        id: 3,
+        img: Mouse,
+        title: "Red mouse",
+        price: 50
+    },
+    {
+        id: 4,
+        img: Iphone,
+        title: "iPhone Xs Max",
+        price: 250
+    },
+    {
+        id: 5,
+        img: Pot,
+        title: "Pureblue pot ",
+        price: 90
+    },
+    {
+        id: 6,
+        img: Mug,
+        title: "Mug with palete",
+        price: 10
+    },
+    {
+        id: 7,
+        img: LuxoryPot,
+        title: "Luxury pot",
+        price: 95
+    },
+    {
+        id: 8,
+        img: WallPot,
+        title: "Wall pot with shape",
+        price: 110
+    },
 ]
 
