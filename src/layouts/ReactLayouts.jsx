84ecf2cf6cd9ac494f3bf0e4a479d9bac5824b07
Yusupov-Ismoiljon <yusupovismoiljon1204@gmail.com>
2024-01-17 11:51:06 +0500

    Login and signup fix

--- src/layouts/ReactLayouts.jsx
+++ src/layouts/ReactLayouts.jsx
@@ -1,12 +1,14 @@
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/Header';
 
-const ReactLayouts = () => {
+const ReactLayouts = ({ searchQuery, setSearchQuery }) => {
     const location = useLocation();
     const header = location.pathname === '/';
     return (
         <div className='flex flex-col'>
-            <Header />
+            {header &&
+                <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+            }
             <main className='max-h-screen'>
                 <Outlet />
             </main>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -5,12 +5,16 @@ import {
     Typography,
 } from "@material-tailwind/react";
 import { Link } from 'react-router-dom';
+import logo from '../../public/img/icon.png'
 
 
-const Signup = ({ signup, setSignup }) => {
+const Signup = () => {
     return (
         <>
-            <div className="bg-star-bg  w-[100%] bg-fixed bg-center">
+            <div className="flex flex-col items-center justify-center w-full h-screen bg-star-bg bg-fixed bg-cover bg-center text-white">
+                <Link className="w-[140px] h-60px]" to={'/'}>
+                    <img src={logo} alt="" />
+                </Link>
                 <div className='flex justify-center items-center '>
                     <div className='w-[450px] border-[#0B0B0B] rounded-lg show py-6 px-7'>
                         <Card className='flex text-center' color="transparent" shadow={false}>
@@ -19,16 +23,16 @@ const Signup = ({ signup, setSignup }) => {
                             </h2>
                             <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
                                 <div className="mb-4 flex flex-col gap-4">
-                                    <Input className='block' size="lg" label="Ism" required />
-                                    <Input className='block' size="lg" label="Familya" required />
-                                    <Input className='block' size="lg" type='number' label="Теlefon" required />
-                                    <Input className='block' size="lg" type='email' label="Pochta" required />
-                                    <Input className='block' type="password" size="lg" label="Parol" required />
+                                    <Input className='block text-white' size="lg" label="Ism" required />
+                                    <Input className='block text-white' size="lg" label="Familya" required />
+                                    <Input className='block text-white' size="lg" type='number' label="Теlefon" required />
+                                    <Input className='block text-white' size="lg" type='email' label="Pochta" required />
+                                    <Input className='block text-white' type="password" size="lg" label="Parol" required />
                                 </div>
                                 <Button type='submit' className="mt-7 bg-[#3b8cdc]" fullWidth>
                                     Ro`yhatdan o`tish
                                 </Button>
-                                <Typography color="gray" className="mt-4 text-center space-x-5 font-normal">
+                                <Typography className="text-white mt-4 text-center space-x-5 font-normal">
                                     <span>
                                         Allaqachon hisobingiz bormi ?
                                     </span>
