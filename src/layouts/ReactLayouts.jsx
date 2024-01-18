d6c2fa9ecaadbe3dd41edebeeaf51d9f257c989a
tolamirzayev-077 <mirodiltolamirzayev5@gmail.com>
2024-01-18 18:56:28 +0500

    Add commit

--- src/layouts/ReactLayouts.jsx
+++ src/layouts/ReactLayouts.jsx
@@ -7,7 +7,7 @@ const ReactLayouts = ({ searchQuery, setSearchQuery }) => {
     return (
         <div className='flex flex-col'>
             {header &&
-                <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+                 <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
             }
             <main className='max-h-screen'>
                 <Outlet />
